86d53620314847477a937eca1f8a952bf2819043
PK22 <PKTwentyTwo@outlook.com>
2026-01-11 19:59:03 +0000

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from .lifetree import lifetree
+
---
+++ lifetree.py
@@ -0,0 +1,427 @@
+'''The code for the lifetree and Pattern classes, which are the highest level components.'''
+import copy
+import os
+import urllib.request
+import hashlib
+try:
+    from .hensel import *
+except ImportError:
+    from hensel import *
+class lifetree:
+    '''Handles and simulates patterns.'''
+    def __init__(self, rule='b3s23'):
+        self.rulehandler = RuleHandler()
+        self.rule = self.rulehandler.canoniserule(rule)
+        self.conditionset = self.rulehandler.makeconditionset(self.rule)
+    def getneighbours(self, grid):
+        '''For each cell with at least one live neighbour, get a 9-bit integer.'''
+        neighbours = {}
+        for x in grid:
+            xcor, ycor = x[0], x[1]
+            for a in range(3):
+                for b in range(3):
+                    coord = (xcor + a - 1, ycor + b - 1)
+                    if coord not in neighbours:
+                        neighbours[coord] = 0
+                    neighbours[coord] += 2**(8 - 3 * b - a)
+        for x in neighbours:
+            if x not in grid:
+                grid[x] = 0
+        return grid, neighbours
+    def advanceone(self, grid):
+        '''Advance a grid of cells by one generation.'''
+        grid, neighbours = self.getneighbours(grid)
+        newgrid = {}
+        for x in neighbours:
+            if neighbours[x] in self.conditionset:
+                newgrid[x] = 1
+        return newgrid
+    def advance(self, grid, gens):
+        '''Advance a grid a specific number of generations.'''
+        for _ in range(gens):
+            grid = self.advanceone(grid)
+        return grid
+    def rle_to_grid(self, rle):
+        '''Converts an RLE to a dictionary format.'''
+        x = 0
+        y = 0
+        grid = {}
+        position = -1
+        digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        cstring = ''
+        isnum = False
+        while position + 1 < len(rle):
+            position += 1
+            if not isnum:
+                if rle[position] == '#':
+                    while rle[position] != '\n' and position < len(rle) - 1:
+                        position += 1
+                    continue
+                if rle[position] in digits:
+                    isnum = True
+                    cstring = rle[position]
+                else:
+                    operator = rle[position]
+                    if operator == '\n':
+                        continue
+                    if cstring != '':
+                        try:
+                            integer = int(cstring)
+                        except:
+                            raise Warning('RLE is incorrectly formatted, defaulting to empty pattern...')
+                            print(cstring)
+                            return {}
+                    else:
+                        integer = 1
+                    match operator:
+                        case 'o':
+                            for n in range(integer):
+                                grid[(x+n, y)] = 1
+                            x += integer
+                        case 'b':
+                            x += integer
+                        case '$':
+                            x = 0
+                            y += integer
+                        case '!':
+                            break
+                    cstring = ''
+            else:
+                if rle[position] not in digits:
+                    isnum = False
+                    position -= 1
+                else:
+                    if rle[position] != '\n':
+                        cstring += rle[position]
+        return grid
+    def grid_to_rle(self, grid, bbox):
+        '''Converts a grid to the RLE of a pattern.'''
+        rows = {}
+        for x, y in grid:
+            if y not in rows:
+                rows[y] = []
+            rows[y].append(x)
+        for x in rows:
+            rows[x] = sorted(rows[x])
+        rows = dict(sorted(rows.items()))
+        rle = 'x = '+str(bbox[2])+', y = '+str(bbox[3])+', rule = '+self.rule.replace('b', 'B').replace('s', '/S')+'\n'
+        for x in range(bbox[1], bbox[1] + bbox[3]):
+            if x not in rows:
+                rle += '$'
+                continue
+            for y in range(bbox[0], bbox[0] + bbox[2]):
+                if y in rows[x]:
+                    rle += 'o'
+                else:
+                    rle += 'b'
+            rle += '$'
+        rle = rle[:-1]
+        rle += '!'
+        #Compress the RLE:
+        operators = ['o', 'b', '$']
+        for x in operators:
+            longestchain = 1
+            while rle.count(x * (longestchain+1)) > 0:
+                longestchain += 1
+            if longestchain >= 2:
+                for n in range(longestchain, 2, -1):
+                    rle = rle.replace(x * n, str(n)+x)
+        return rle
+    def hashsoup(self, instring, sym):
+        '''Generates a soup based on the instring, returning a Pattern.'''
+        #I borrowed this function from apgsearch Py3 - see the repo (https://github.com/PKTwentyTwo/apgsearch-Py3) for the credits for this function.
+        s = hashlib.sha256(instring.encode('utf-8')).digest()
+        thesoup = []
+        if sym in ['D2_x', 'D8_1', 'D8_4']:
+            d = 1
+        elif sym in ['D4_x1', 'D4_x4']:
+            d = 2
+        else:
+            d = 0
+            
+        for j in range(32):
+
+            t = (s[j])
+
+            for k in range(8):
+
+                if (sym in ['8x32']):
+                    
+                    x = k + 8*(j % 4)
+                    y = int(j / 4)
+                    
+                elif (sym in ['4x64']):
+                    
+                    x = k + 8*(j % 8)
+                    y = int(j / 8)
+            
+                elif (sym in ['2x128']):
+                    
+                    x = k + 8*(j % 16)
+                    y = int(j / 16)
+                    
+                elif (sym in ['1x256']):
+                    
+                    x = k + 8*(j % 32)
+                    y = int(j / 32)
+                    
+                else:
+                    
+                    x = k + 8*(j % 2)
+                    y = int(j / 2)
+
+                if (t & (1 << (7 - k))):
+                    
+                    if ((d == 0) | (x >= y)):
+
+                        thesoup.append(x)
+                        thesoup.append(y)
+                    elif (sym in ['D4_x1']):
+
+                        thesoup.append(y)
+                        thesoup.append(-x)
+
+                    elif (sym in ['D4_x4']):
+
+                        thesoup.append(y)
+                        thesoup.append(-x-1)
+                    if ((sym in ['D4_x1']) & (x == y)):
+
+                        thesoup.append(y)
+                        thesoup.append(-x)
+
+                    if ((sym in ['D4_x4']) & (x == y)):
+
+                        thesoup.append(y)
+                        thesoup.append(-x-1)
+
+        # Checks for diagonal symmetries:
+        if (d >= 1):
+            for x in range(0, len(thesoup), 2):
+                thesoup.append(thesoup[x+1])
+                thesoup.append(thesoup[x])
+            if d == 2:
+                if (sym == 'D4_x1'):
+                    for x in range(0, len(thesoup), 2):
+                        thesoup.append(-thesoup[x+1])
+                        thesoup.append(-thesoup[x])
+                else:
+                    for x in range(0, len(thesoup), 2):
+                        thesoup.append(-thesoup[x+1] - 1)
+                        thesoup.append(-thesoup[x] - 1)
+                for i in range(inflationamount):
+                    thenewsoup = []
+                    for x in range(len(thesoup)//2):
+                        thenewsoup = thenewsoup + [thesoup[x*2]*2, thesoup[x*2+1]*2,thesoup[x*2]*2+1, thesoup[x*2+1]*2,thesoup[x*2]*2, thesoup[x*2+1]*2+1,thesoup[x*2]*2+1, thesoup[x*2+1]*2+1]
+                    thesoup = thenewsoup
+                return thesoup
+
+        # Checks for orthogonal x symmetry:
+        if sym in ['D2_+1', 'D4_+1', 'D4_+2']:
+            for x in range(0, len(thesoup), 2):
+                thesoup.append(thesoup[x])
+                thesoup.append(-thesoup[x+1])
+        elif sym in ['D2_+2', 'D4_+4']:
+            for x in range(0, len(thesoup), 2):
+                thesoup.append(thesoup[x])
+                thesoup.append(-thesoup[x+1] - 1)
+
+        # Checks for orthogonal y symmetry:
+        if sym in ['D4_+1']:
+            for x in range(0, len(thesoup), 2):
+                thesoup.append(-thesoup[x])
+                thesoup.append(thesoup[x+1])
+        elif sym in ['D4_+2', 'D4_+4']:
+            for x in range(0, len(thesoup), 2):
+                thesoup.append(-thesoup[x] - 1)
+                thesoup.append(thesoup[x+1])
+
+
+        # Checks for rotate2 symmetry:
+        if sym in ['C2_1', 'C4_1', 'D8_1']:
+            for x in range(0, len(thesoup), 2):
+                thesoup.append(-thesoup[x])
+                thesoup.append(-thesoup[x+1])
+        elif sym in ['C2_2']:
+            for x in range(0, len(thesoup), 2):
+                thesoup.append(-thesoup[x])
+                thesoup.append(-thesoup[x+1]-1)
+        elif sym in ['C2_4', 'C4_4', 'D8_4']:
+            for x in range(0, len(thesoup), 2):
+                thesoup.append(-thesoup[x]-1)
+                thesoup.append(-thesoup[x+1]-1)
+
+        # Checks for rotate4 symmetry:
+        if (sym in ['C4_1', 'D8_1']):
+            for x in range(0, len(thesoup), 2):
+                thesoup.append(thesoup[x+1])
+                thesoup.append(-thesoup[x])
+        elif (sym in ['C4_4', 'D8_4']):
+            for x in range(0, len(thesoup), 2):
+                thesoup.append(thesoup[x+1])
+                thesoup.append(-thesoup[x]-1)
+        inflationamount = 0
+        while sym.startswith((inflationamount + 1)*'i'):
+            inflationamount += 1
+        for i in range(inflationamount):
+            thenewsoup = []
+            for x in range(len(thesoup)//2):
+                thenewsoup = thenewsoup + [thesoup[x*2]*2, thesoup[x*2+1]*2,thesoup[x*2]*2+1, thesoup[x*2+1]*2,thesoup[x*2]*2, thesoup[x*2+1]*2+1,thesoup[x*2]*2+1, thesoup[x*2+1]*2+1]
+            thesoup = thenewsoup
+        thesoup2 = {}
+        for x in range(len(thesoup)//2):
+            thesoup2[(thesoup[2*x], thesoup[2*x+1])] = 1
+        return self.pattern(thesoup2)
+
+    def download_synth(self, apgcode):
+        '''Downloads a glider synthesis from Catagolue.'''
+        if self.rule != 'b3s23':
+            raise ValueError('Can only download syntheses if configured for b3s23.')
+        URL = 'https://catagolue.hatsya.com'
+        c = urllib.request.urlopen(URL + '/textsamples/' + apgcode + '/' + 'b3s23/synthesis')
+        response = c.read().decode('utf-8')
+        if 'x' in response:
+            return response
+        else:
+            return None
+        
+    def pattern(self, rle_or_grid):
+        '''Creates a new Pattern given an RLE string.'''
+        if type(rle_or_grid) == type(''):
+            grid = self.rle_to_grid(rle)
+        else:
+            grid = rle_or_grid
+        pt = Pattern(self, grid)
+        return pt
+class Pattern:
+    '''This is the class used for manipulation of patterns.'''
+    def __init__(self, lifetree, grid={}):
+        '''This method should only be called by a lifetree.'''
+        self.lifetree = lifetree
+        self.grid = grid
+    def __getitem__(self, gens):
+        '''Advances a pattern a given number of generations.'''
+        self2 = self.clone()
+        self2.grid = self2.lifetree.advance(self.grid, gens)
+        return self2
+    def __call__(self, *args):
+        '''Translates or transforms a pattern.'''
+        if len(args) == 2:
+            return self.move(args[0], args[1])
+    def transform(self, transformation):
+        '''Transforms a pattern relative to the origin.'''
+        #It's currently not possible to go from any orientation to
+        #any other orientation with just one transformation.
+        transformations = ['flip_x', 'flip_y', 'identity', 'rot_90', 'rot_180', 'rot_270', 'flip_xy', 'rcw', 'rccw']
+        if transformation not in transformations:
+            raise ValueError('Only the following transformations are supported: '+str(transformations))
+        pt2 = self.clone()
+        grid = pt2.grid
+        newgrid = {}
+        match transformation:
+            case 'identity':
+                pass
+            case 'flip_x':
+                for x,y in grid:
+                    newgrid[(-x,y)] = 1
+            case 'flip_y':
+                for x,y in grid:
+                    newgrid[(x,-y)] = 1
+            case 'flip_xy':
+                for x,y in grid:
+                    newgrid[(-x,-y)] = 1
+            case 'rot_90':
+                for x,y in grid:
+                    newgrid[(y, -x)] = 1
+            case 'rot_180':
+                for x,y in grid:
+                    newgrid[(-x,-y)] = 1
+            case 'rot_270':
+                for x,y in grid:
+                    newgrid[(-y, x)] = 1
+            case 'rcw':
+                for x,y in grid:
+                    newgrid[(y, -x)] = 1
+            case 'rccw':
+                for x,y in grid:
+                    newgrid[(-y, x)] = 1
+        pt2.grid = newgrid
+        return pt2
+    def clone(self):
+        '''Creates a copy of a lifetree.'''
+        thecopy = copy.deepcopy(self)
+        thecopy.cleanup()
+        return thecopy
+    def cleanup(self):
+        '''Cleans up the stored data of a lifetree.'''
+        toremove = []
+        for x in self.grid:
+            if self.grid[x] == 0:
+                toremove.append(x)
+        for x in toremove:
+            self.grid.pop(x)
+    def move(self, dx, dy):
+        '''Translates a pattern by (dx, dy).'''
+        self2 = self.clone()
+        newgrid = {}
+        for x in self2.grid:
+            newgrid[x[0] + dx, x[1] + dy] = 1
+        self2.grid = newgrid
+        self2.cleanup()
+        return self2
+
+    @property
+    def rle(self):
+        '''The Run Length Encoding (RLE) of a pattern.'''
+        self.cleanup()
+        return self.lifetree.grid_to_rle(self.grid, self.bbox)
+    @property
+    def population(self):
+        '''How many live cells a pattern has.'''
+        self.cleanup()
+        return len(self.grid)
+    @property
+    def coords(self):
+        '''A list of every cell in a pattern.'''
+        self.cleanup()
+        thecoords = []
+        for x in self.grid:
+            thecoords.append(x)
+        return thecoords
+    @property
+    def firstcell(self):
+        '''The first cell of a pattern.'''
+        self.grid = dict(sorted(self.grid.items()))
+        count = 0
+        for x in self.grid:
+            count += 1
+            if count == 1:
+                return x
+        return None
+    @property
+    def bbox(self):
+        '''The bounding box of a pattern in the form [x, y, dx, dy].'''
+        cells = self.coords
+        if len(cells) == 0:
+            #Empty patterns do not have a proper bounding box.
+            return None
+        exes = [c[0] for c in cells]
+        whys = [c[1] for c in cells]
+
+        x = min(exes)
+        y = min(whys)
+        dx = max(exes) - x + 1
+        dy = max(whys) - y + 1
+        return [x, y, dx, dy]
+##lt = Lifetree('B3/S23')
+##pt = lt.pattern('o2b$3o$bo!')
+##print(pt.population)
+##pt2 = pt[100]
+##print(pt.population)
+##print(pt2.population)
+##grid = {(10,10):1,(11,10):1,(10,11):1,(9,10):1,(9,9):1}
+##timer = stopwatch.Stopwatch()
+##timer.start()
+##grid = lt.advance(grid, 1103)
+##print(timer.stop())
+##print(len(grid))
