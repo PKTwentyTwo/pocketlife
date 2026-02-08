e28111f2e44110d11a5e45e9d5113a969466a12e
PK22 <PKTwentyTwo@outlook.com>
2026-02-08 20:17:16 +0000

    Cython built libraries implemented into module.

--- __init__.py
+++ __init__.py
@@ -1,2 +1,18 @@
 '''A Python package for manipulation of patterns in cellular automata. '''
-from .lifetree import Lifetree as lifetree
+import os
+#Check for .so and .pyd files:
+localfiles = os.listdir(os.getcwd())
+localfiles = [x for x in localfiles if x.endswith('.pyd') or x.endswith('.so')]
+if 'pylifetree.so' in localfiles or 'pylifetree.pyd' in localfiles:
+    try:
+        from pylifetree import Lifetree
+        lifetree = Lifetree
+    except ImportError:
+        raise Warning('''Failed to import from compiled package!
+Try running cython_compile() or remove_cython_compilation()''')
+        from .lifetree import Lifetree as lifetree
+else:
+    from .lifetree import Lifetree as lifetree
+#Import the compiler regardless:
+from .cython_setup import cython_compile, remove_cython_compilation
+__all__ = ['lifetree', 'cython_compile', 'remove_cython_compilation']
--- cython_setup.py
+++ cython_setup.py
@@ -1,45 +1,67 @@
 '''Used to compile a .so or .pyd to improve speed.'''
 from setuptools import setup, Extension
 from Cython.Build import cythonize
-import platform
 import os
 import sys
-#Create a .pyx file:
-with open('lifetree.py', 'r', encoding='utf-8') as f:
-    code = f.read()
-with open('pylifetree.pyx', 'w', encoding='utf-8') as f:
-    f.write(code)
-extensions = [
-    Extension(
-        name="pylifetree",
-        sources=["pylifetree.pyx"],
-        language="c",
-    )
-]
+def cython_compile():
+    '''Compiles a Cython library to improve speed.
+Requires that Cython is installed as a Python package.'''
+    executable = sys.executable
+    cmd = executable + ' ' + __file__ + ' build_ext --inplace'
+    print('Attempting to compile using following command:')
+    print(cmd)
+    os.system(cmd)
+def remove_cython_compilation():
+    '''Deletes any compiled Cython libraries.'''
+    dirloc = os.path.dirname(__file__)
+    files = os.listdir(dirloc)
+    files = [x for x in files if x.endswith('.so') or x.endswith('.pyd')]
+    total = 0
+    for n in files:
+        total += 1
+        os.remove(dirloc + '/' + n)
+    print('Successfully removed '+str(total)+' files.')
+if __name__ == '__main__':
+    #Create a .pyx file:
+    oldcwd = os.getcwd()
+    os.chdir(os.path.dirname(__file__))
+    with open('lifetree.py', 'r', encoding='utf-8') as f:
+        code = f.read()
+    with open('pylifetree.pyx', 'w', encoding='utf-8') as f:
+        f.write(code)
+    extensions = [
+        Extension(
+            name="pylifetree",
+            sources=["pylifetree.pyx"],
+            language="c",
+        )
+    ]
 
-setup(
-    name="pylifetree",
-    ext_modules=cythonize(
-        extensions,
-        compiler_directives={
-            "language_level": "3",
-            "boundscheck": False,
-            "wraparound": False,
-        }
-    ),
-)
-#Rename the output file:
-files = os.listdir(os.getcwd())
-files = [x for x in files if x.endswith('.so') or x.endswith('.pyd')]
-if len(files) == 0:
-    raise FileNotFoundError('Could not locate compiled .so or .pyd!')
-file = files[0]
-if file.endswith('.so'):
-    extension = '.so'
-else:
-    extension = '.pyd'
-with open(file, 'rb') as f:
-    code = f.read()
-os.remove(file)
-with open('pylifetree' + extension, 'wb') as f:
-    f.write(code)
+    setup(
+        name="pylifetree",
+        ext_modules=cythonize(
+            extensions,
+            compiler_directives={
+                "language_level": "3",
+                "boundscheck": False,
+                "wraparound": False,
+            }
+        ),
+    )
+    #Rename the output file:
+    files = os.listdir(os.getcwd())
+    files = [x for x in files if x.endswith('.so') or x.endswith('.pyd')]
+    if len(files) == 0:
+        raise FileNotFoundError('Could not locate compiled .so or .pyd!')
+    file = files[0]
+    if file.endswith('.so'):
+        extension = '.so'
+    else:
+        extension = '.pyd'
+    with open(file, 'rb') as f:
+        code = f.read()
+    os.remove(file)
+    with open('pylifetree' + extension, 'wb') as f:
+        f.write(code)
+    os.chdir(oldcwd)
+
